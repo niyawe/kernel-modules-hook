79c490dbfbb8921bb7f3396421fdb54965e15e25
Niklas Yann Wettengel <niyawe@niyawe.de>
2022-10-13 22:14:05 +0200

    mount at subfolder

--- kernel-modules-backup-post.sh
+++ kernel-modules-backup-post.sh
@@ -3,10 +3,12 @@
 KVER="${KVER:-$(uname -r)}"
 TMPDIR=/tmp/kernel-module-backup
 if [[ -e "$TMPDIR" ]]; then
+    echo "creating directory"
+    mkdir /usr/lib/modules/${KVER}
     echo "mounting tmpfs"
-    mount -t tmpfs tmpfs /usr/lib/modules
+    mount -t tmpfs tmpfs /usr/lib/modules/${KVER}
     echo "moving modules to tmpfs"
-    mv -n "$TMPDIR/$KVER" /usr/lib/modules
+    mv -n "$TMPDIR/$KVER/*" /usr/lib/modules/${KVER}
     echo "removing modules from /tmp"
     rm -r $TMPDIR
 fi
--- kernel-modules-backup-pre.sh
+++ kernel-modules-backup-pre.sh
@@ -2,12 +2,11 @@
 
 KVER="${KVER:-$(uname -r)}"
 if [[ -e "/usr/lib/modules/${KVER}" ]]; then
+    if mountpoint "/usr/lib/modules/${KVER}"; then
+        exit 0
+    fi
     TMPDIR=/tmp/kernel-module-backup
     mkdir -p $TMPDIR
     echo "copying modules"
     cp -a "/usr/lib/modules/${KVER}" $TMPDIR
-    if findmnt -t tmpfs /usr/lib/modules > /dev/null; then
-        echo "unmounting /usr/lib/modules"
-        umount /usr/lib/modules
-    fi
 fi
